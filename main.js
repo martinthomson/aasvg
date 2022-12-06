88da7986418ef6bffc111ebb944942a54bac7a53
Martin Thomson <mt@lowentropy.net>
2022-12-06 16:49:46 +1100

    Bump version

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 const { diagramToSVG } = require("./markdeep-diagram.js");
-const VERSION = "aasvg 0.3.3";
+const VERSION = "aasvg 0.3.4";
 
 function usage() {
     console.warn("Turn ASCII art into SVG");

1c3e1177d248a2792494b885850afe0ef39df355
Martin Thomson <mt@lowentropy.net>
2024-01-12 13:02:46 +1100

    Release for squigglies

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 const { diagramToSVG } = require("./markdeep-diagram.js");
-const VERSION = "aasvg 0.3.9";
+const VERSION = "aasvg 0.4.0";
 
 function usage() {
     console.warn("Turn ASCII art into SVG");
